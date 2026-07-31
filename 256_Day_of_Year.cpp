389a69d12bf38ea3b01f92d77e9be577df26aea5
Yamuna988 <yamunatanuku079@gmail.com>
2026-07-31 14:18:36 +0530

    Add files via upload
    Day of the Programmer
    
    Problem Understanding:
    Given a year between 1700 and 2700, find the date of the 256th day of that year according to the official Russian calendar.
    
    Approach:
    
    For years from 1700 to 1917, use the Julian calendar.
    For years from 1919 onwards, use the Gregorian calendar.
    1918 is a special transition year, and its 256th day is September 26.
    Check whether the given year is a leap year.
    A normal year gives 13.09, while a leap year gives 12.09.
    Finally, print the date in dd.mm.yyyy format.

---
+++ 256_Day_of_Year.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+char* dayOfProgrammer(int year)
+{
+    static char date[20];
+
+    // Special case: 1918
+    if (year == 1918)
+    {
+        sprintf(date, "26.09.%d", year);
+    }
+
+    // Julian calendar
+    else if (year < 1918)
+    {
+        if (year % 4 == 0)
+            sprintf(date, "12.09.%d", year);
+        else
+            sprintf(date, "13.09.%d", year);
+    }
+
+    // Gregorian calendar
+    else
+    {
+        if ((year % 400 == 0) ||
+            (year % 4 == 0 && year % 100 != 0))
+            sprintf(date, "12.09.%d", year);
+        else
+            sprintf(date, "13.09.%d", year);
+    }
+
+    return date;
+}
+
+int main()
+{
+    int year;
+    scanf("%d", &year);
+
+    printf("%s", dayOfProgrammer(year));
+
+    return 0;
+}
